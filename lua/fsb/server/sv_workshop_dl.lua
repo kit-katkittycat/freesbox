0fdc6152f2d7c19ce767ac38caa74f903456c0f1
Kit <102991955+kit-katkittycat@users.noreply.github.com>
2026-09-08 05:37:14 +0000

    chore: Cleanup Rebase litter

--- lua/fsb/server/sv_workshop_dl.lua
+++ lua/fsb/server/sv_workshop_dl.lua
@@ -1,40 +1,4 @@
 -- Add workshop content for the clients.
-<<<<<<< HEAD
-
-resource.AddWorkshop("737640184") -- Tank track
-resource.AddWorkshop("160250458") -- Wiremod
-resource.AddWorkshop("2802133674") -- Petition swep content
-
---#region Petitioned addons
-resource.AddWorkshop("3389728250") -- Glide
-resource.AddWorkshop("3389795738") -- Glide // GTAV: Helicopters
-resource.AddWorkshop("3673232532") -- Glide // Extra Details
-resource.AddWorkshop("3620753374") -- Left 4 Dead // Glide 
-resource.AddWorkshop("246756300") -- 3D Stream Radio
-resource.AddWorkshop("2675972006") -- Custom Loadout
-resource.AddWorkshop("3318060741") -- rRadio
-resource.AddWorkshop("104575630") -- Ragdoll Mover
-resource.AddWorkshop("109643223") -- 3D2D Textscreens
-resource.AddWorkshop("2840295308") -- Primitive
-resource.AddWorkshop("2458909924") -- Prop2Mesh
-resource.AddWorkshop("287012681") -- Track Assembly Tool
-resource.AddWorkshop("2639959090") -- Manable Emplacements
-resource.AddWorkshop("3687687491") -- Glide // Oppressor Mk2
-resource.AddWorkshop("3001397905") -- Media Player (Updated Edition)
-resource.AddWorkshop("104691717") -- PAC3
-resource.AddWorkshop("2656563609") -- Musical Keyboard
-resource.AddWorkshop("173482196") -- SProps Workshop Edition
-resource.AddWorkshop("2821862386") -- Simple Weapons: Base
-resource.AddWorkshop("2821865508") -- Simple Weapons: CS:S
-resource.AddWorkshop("2775687029") -- [ ULTRA GUNS ] ГАНТЕЛЯ 2-4 КИЛЛОГРАММА!!!!!! swep
-resource.AddWorkshop("3620516732") -- Glide // Styled's Experiments
-resource.AddWorkshop("2175553724") -- Wiremod Extras
-resource.AddWorkshop("3442302711") -- [G]VRMod: Ultimate
-resource.AddWorkshop("1517464837") -- ttt_cigarette
-resource.AddWorkshop("2853616849") -- Heineken Beer SWEP
-resource.AddWorkshop("2962299500") -- Synergy Elite Buggy [Vehicle]
---#endregion
-=======
 local WORKSHOP_ITEMS = {
 	"737640184", -- Tank track
 	"160250458", -- Wiremod
@@ -72,4 +36,3 @@ local WORKSHOP_ITEMS = {
 for _,id in ipairs(WORKSHOP_ITEMS) do
 	resource.AddWorkshop(id)
 end
->>>>>>> 72d3874 (refactor(Workshop): Change system to for loop)
